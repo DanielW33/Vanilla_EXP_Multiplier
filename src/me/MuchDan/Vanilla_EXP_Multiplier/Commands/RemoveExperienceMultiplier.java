0e210ff8d1b08f8376e907ce91d7694a334e45fa
Daniel Williams <59080478+DanielW33@users.noreply.github.com>
2020-11-06 02:46:33 -0500

    Add files via upload
    Full Project Upload

---
+++ src/me/MuchDan/Vanilla_EXP_Multiplier/Commands/RemoveExperienceMultiplier.java
@@ -0,0 +1,61 @@
+package me.MuchDan.Vanilla_EXP_Multiplier.Commands;
+
+import me.MuchDan.Vanilla_EXP_Multiplier.ConfigManagers.PlayerEXPMultiplierConfigManager;
+import me.MuchDan.Vanilla_EXP_Multiplier.ConfigManagers.PluginConfigurationManagerVEM;
+import me.MuchDan.Vanilla_EXP_Multiplier.Vanilla_EXP_Multiplier;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class RemoveExperienceMultiplier implements CommandExecutor {
+    private Vanilla_EXP_Multiplier plugin;
+    private PluginConfigurationManagerVEM ConfigFile;
+    private PlayerEXPMultiplierConfigManager PlayerData;
+
+    public RemoveExperienceMultiplier(Vanilla_EXP_Multiplier plugin) {
+        this.plugin = plugin;
+        this.ConfigFile = plugin.getConfigFile();
+        this.PlayerData = plugin.getBoostFile();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender Sender, Command cmd, String Label, String[] args) {
+        if (Label.equalsIgnoreCase("removeMultiplier")) {
+            String Permission = ConfigFile.getConfig().getString("Permission");
+            if (!Sender.hasPermission(Permission)) {
+                String Deny = ConfigFile.getConfig().getString("DenyMessage");
+                Sender.sendMessage(ChatColor.translateAlternateColorCodes('&', Deny));
+                return false;
+            }
+            if (args.length < 1) {
+                Sender.sendMessage(ChatColor.GREEN + "Command usage: /removeMultiplier [Username]");
+                return false;
+            }
+
+            Player player = Bukkit.getPlayerExact(args[0]);
+
+            if(CheckPlayerData(player)){
+                Sender.sendMessage(ChatColor.GREEN + "Successfully removed " + player.getName() + "'s Multiplier.");
+            }
+            else{
+                Sender.sendMessage(ChatColor.RED + "Failed to remove " + player.getName() + "'s Multiplier... You sure they have an active Multiplier?");
+            }
+        }
+        return true;
+    }
+
+    public boolean CheckPlayerData(Player player) {
+        String UUID = player.getUniqueId().toString();
+        if (PlayerData.getConfig().contains("Players." + UUID)) {
+            PlayerData.getConfig().set("Players." + UUID + ".Multiplier", null);
+            PlayerData.getConfig().set("Players." + UUID + ".Name", null);
+            PlayerData.getConfig().set("Players." + UUID, null);
+            PlayerData.saveConfig();
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/me/MuchDan/Vanilla_EXP_Multiplier/ConfigManagers/PluginConfigurationManagerVEM.java
@@ -0,0 +1,65 @@
+package me.MuchDan.Vanilla_EXP_Multiplier.ConfigManagers;
+
+import me.MuchDan.Vanilla_EXP_Multiplier.Vanilla_EXP_Multiplier;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+public class PluginConfigurationManagerVEM {
+
+    private Vanilla_EXP_Multiplier plugin;
+    private FileConfiguration dataConfig = null;
+    private File configFile = null;
+
+    public PluginConfigurationManagerVEM(Vanilla_EXP_Multiplier plugin) {
+        this.plugin = plugin;
+        saveDefaultConfig();
+    }
+
+    public void reloadConfig() {
+        if (this.configFile == null) {
+            this.configFile = new File(this.plugin.getDataFolder(), "MultiplierConfig.yml");
+        }
+        this.dataConfig = YamlConfiguration.loadConfiguration(this.configFile);
+
+        InputStream defaultStream = this.plugin.getResource("MultiplierConfig.yml");
+        if (defaultStream != null) {
+            YamlConfiguration defaultConfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.dataConfig.setDefaults((defaultConfig));
+        }
+    }
+
+    public FileConfiguration getConfig() {
+        if (this.dataConfig == null) {
+            reloadConfig();
+        }
+        return this.dataConfig;
+    }
+
+    public void saveConfig() {
+        if (this.dataConfig == null || this.configFile == null) {
+            return;
+        }
+        try {
+            this.getConfig().save(this.configFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.SEVERE, "Failed to save data config.", e);
+            e.printStackTrace();
+        }
+    }
+
+    public void saveDefaultConfig() {
+        if (this.configFile == null) {
+            this.configFile = new File(this.plugin.getDataFolder(), "MultiplierConfig.yml");
+        }
+
+        if (!this.configFile.exists()) {
+            this.plugin.saveResource("MultiplierConfig.yml", false);
+        }
+    }
+}
